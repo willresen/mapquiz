1d8aeb0edfb6875301661fa596421d017800c7fa
Will Resen <willresen@gmail.com>
2019-07-01 21:24:37 -0700

    Configure Express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static( __dirname + '/../client/dist/'));
+app.use(express.json());
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
