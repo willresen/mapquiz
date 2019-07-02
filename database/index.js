29952022c1c887d99c1567dec25e5fd5924bd73b
Will Resen <willresen@gmail.com>
2019-07-01 22:15:48 -0700

    Implement MongoDB query functions

---
+++ database/index.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+
+/***********
+ * SCHEMAS *
+ ***********/
+
+const markerSchema = new mongoose.Schema({
+  name: 'string',
+  id: 'string',
+  last_used: 'date',
+  count: 'number'
+}, {strict: true, strictQuery: true});
+
+const quizSchema = new mongoose.Schema({
+  marker_positions: 'mixed'
+});
+
+/**********
+ * MODELS *
+ **********/
+
+const Marker = mongoose.model('Marker', markerSchema);
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+/***********
+ * QUERIES *
+ ***********/
+
+const getMarker = (id) => {
+  return Marker.find({id: id});
+};
+
+const saveMarker = (marker) => {
+  return Marker.findOneAndUpdate({id: marker.id}, marker, {upsert: true, new: true});
+};
+
+const getQuiz = (id) => {
+  return Quiz.find({_id: id});
+};
+
+const saveQuiz = (quiz) => {
+  return Quiz.findOneAndUpdate({_id: quiz.id}, quiz, {upsert: true, new: true});
+};
+
+module.exports.saveQuiz = saveQuiz;
+module.exports.getQuiz = getQuiz;
+module.exports.getMarker = getMarker;
+module.exports.saveMarker = saveMarker;
