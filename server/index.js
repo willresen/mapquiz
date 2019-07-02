bcb1b4747b16c902ba928cdbc63acecebaae4df9
Will Resen <willresen@gmail.com>
2019-07-01 21:43:04 -0700

    Setup RESTful routes

--- server/index.js
+++ server/index.js
@@ -6,5 +6,20 @@ const port = 3000;
 app.use(express.static( __dirname + '/../client/dist/'));
 app.use(express.json());
 
+app.get('/api/locations', (req, res) => {
+  //retrieve a marker from the database
+})
+
+app.post('/api/locations', (req, res) => {
+  //add a marker to the database
+})
+
+app.get('/api/quizzes', (req, res) => {
+  //retrieve a saved quiz from the database
+})
+
+app.post('/api/quizzes', (req, res) => {
+  //save a quiz to the database
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
