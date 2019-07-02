d9e67aeafb3e3aff37e04670248344dcd53a70bf
Will Resen <willresen@gmail.com>
2019-07-01 21:53:46 -0700

    Create routes to db

--- server/index.js
+++ server/index.js
@@ -1,25 +1,38 @@
 require('dotenv').config();
 const express = require('express');
+const db = require(__dirname + '/../database/index.js');
 const app = express();
 const port = 3000;
 
 app.use(express.static( __dirname + '/../client/dist/'));
 app.use(express.json());
 
+//retrieve a marker from the database
 app.get('/api/locations', (req, res) => {
-  //retrieve a marker from the database
+  db.getMarker(req.body.location)
+    .then(results => res.send(results))
+    .catch(err => console.log(err))
 })
 
+//add a marker to the database
 app.post('/api/locations', (req, res) => {
-  //add a marker to the database
+  db.saveMarker(req.body.location)
+  .then(results => res.send(results))
+  .catch(err => console.log(err))
 })
 
+//retrieve a saved quiz from the database
 app.get('/api/quizzes', (req, res) => {
-  //retrieve a saved quiz from the database
+  db.getQuiz(req.body.quiz)
+  .then(results => res.send(results))
+  .catch(err => console.log(err))
 })
 
+//save a quiz to the database
 app.post('/api/quizzes', (req, res) => {
-  //save a quiz to the database
+  db.saveQuiz(req.body.quiz)
+  .then(results => res.send(results))
+  .catch(err => console.log(err))
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
