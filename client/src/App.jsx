96ac97d76a1cd0518993e34e0a7386b707085195
Will Resen <willresen@gmail.com>
2019-08-14 00:39:20 -0700

    Pass props to Overlay and Sidebar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './components/Map.jsx';
+import MapContainer from './components/Map.jsx';
 import Overlay from './components/Overlay.jsx';
 import Status from './components/Status.jsx';
 import Sidebar from './components/Sidebar.jsx';
@@ -14,16 +14,38 @@ class App extends React.Component{
       saved: false,
       loaded: false,
     }
+    this.closePopup = this.closePopup.bind(this);
+    this.fetchMarkers = this.fetchMarkers.bind(this);
+  }
+
+  closePopup(popup, overlay) {
+    document.getElementById(popup).classList.remove("w3-animate-zoom");
+    void document.getElementById(popup).offsetWidth;
+    document.getElementById(popup).classList.add("w3-animate-zoomout");
+    setTimeout(function () {
+      document.getElementById(overlay).style.display = "none";
+      document.getElementById(popup).style.display = "none";
+    }, 0)
+  }
+
+  fetchMarkers(locations) {
+    fetch('/api/locations/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
+      body: locations
+    })
+      .then(result => result.json())
+      .then(result => this.setState({ markers: result }));
   }
 
   render() {
     return (
       <div id="wrapper">
-        {/* <Status /> */}
+        <Status />
         <div id="main">
-          <Overlay />
-          <Sidebar />
-          <Map />
+          <Overlay closePopup={this.closePopup}/>
+          <Sidebar fetchMarkers={this.fetchMarkers}/>
+          <MapContainer />
         </div>
       </div>
     )
