fd1756b15acfbce0ca8169757f482394ed8a01f0
Will Resen <willresen@gmail.com>
2019-08-14 00:37:13 -0700

    Add redis and react-dom to dependencies

--- server/index.js
+++ server/index.js
@@ -1,39 +1,40 @@
 require('dotenv').config();
 const express = require('express');
-const db = require(__dirname + '/../database/index.js');
+const axios = require('axios');
+const Promise = require('bluebird');
+Promise.promisifyAll(require("redis"));
+const redis = require('redis').createClient(6379);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static( __dirname + '/../client/dist/'));
 app.use('/legacy', express.static(__dirname + '/../legacy/'))
-app.use(express.json());
+app.use(express.text());
 
-//retrieve a marker from the database
-app.get('/api/locations', (req, res) => {
-  db.getMarker(req.body.location)
-    .then(results => res.send(results))
-    .catch(err => console.log(err))
+app.get('/api/locations/', (req, res) => {
 })
 
 //add a marker to the database
-app.post('/api/locations', (req, res) => {
-  db.saveMarker(req.body.location)
-  .then(results => res.send(results))
-  .catch(err => console.log(err))
-})
+app.post('/api/locations', async (req, res) => {
+  const locations = req.body.split('\n');
+  let markers = [];
 
-//retrieve a saved quiz from the database
-app.get('/api/quizzes', (req, res) => {
-  db.getQuiz(req.body.id)
-  .then(results => res.send(results))
-  .catch(err => console.log(err))
-})
+  for (location of locations) {
+    await redis.getAsync(`location:${location}`)
+      .then(results => markers.push({ source: 'cache', location: location, results: results }))
+      .catch(() => {
+        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`)
+          .then(response => {
+            response = response.data.results[0].geometry.location;
+            redis.setex(`location:${location}`, 2592000, JSON.stringify(response));
+            markers.push({ source: 'google', location: location, results: response });
+          })
+          .catch(err => res.status(404))
+      });
+  }
+  res.send(markers)
+});
 
-//save a quiz to the database
-app.post('/api/quizzes', (req, res) => {
-  db.saveQuiz(req.body.quiz)
-  .then(results => res.send(results))
-  .catch(err => console.log(err))
-})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
