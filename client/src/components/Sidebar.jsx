ee0f483df9454db042605a40413b1aadfd8e58ca
Will Resen <willresen@gmail.com>
2019-08-14 00:41:48 -0700

    Convert to stateful component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 
-const toggleSidebar = () => {
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-};
+  handleChange(e) {
+    this.setState({locations: e.target.value})
+  }
 
-const closePopup = () => {
-
-};
-
-const Sidebar = () => {
-  return (
-    <div id="sidebar">
-      <div id="sidebar_toggle" onClick={toggleSidebar}>. . . . .</div>
-      <div id="sidebar_contents" style={{display: "block"}}>
-        <textarea name="comment" id="address" rows="8" placeholder="Enter one location per line"></textarea>
-
-        <div id="control_buttons">
-          <button className="button" id="createnewquiz">Create Quiz</button><br></br>
-          <button className="button" id="savequiz">Save</button>
-          <button className="button" onClick={() => { closePopup('instructions', 'mapoverlay'); openPopup('load', 'overlay')} }
-            id="load_tab_button">Load</button>
+  render() {
+    return (
+      <div id="sidebar">
+        <div id="sidebar_contents">
+          <textarea
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.locations}
+            id="address"
+            rows="8"
+            placeholder="Enter one location per line">
+          </textarea>
+          <div id="control_buttons">
+            <button id="createnewquiz" onClick={() => this.props.fetchMarkers(this.state.locations)}>Create Quiz</button><br></br>
+            <button id="savequiz">Save</button>
+            <button onClick={() => { closePopup('instructions', 'mapoverlay'); openPopup('load', 'overlay') }}
+              id="load_tab_button">Load</button>
+          </div>
         </div>
-
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default Sidebar;
