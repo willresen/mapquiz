6913167c65c87265a7b0d53fcbd25d9712718005
Will Resen <willresen@gmail.com>
2019-08-14 13:37:49 -0700

    Convert to stateless component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      locations: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({locations: e.target.value})
-  }
-
-  render() {
-    return (
-      <div id="sidebar">
-        <div id="sidebar_contents">
-          <textarea
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.locations}
-            id="address"
-            rows="8"
-            placeholder="Enter one location per line">
-          </textarea>
-          <div id="control_buttons">
-            <button id="createnewquiz" onClick={() => this.props.fetchMarkers(this.state.locations)}>Create Quiz</button><br></br>
-            <button id="savequiz">Save</button>
-            <button onClick={() => { closePopup('instructions', 'mapoverlay'); openPopup('load', 'overlay') }}
-              id="load_tab_button">Load</button>
-          </div>
-        </div>
+const Sidebar = (props) => (
+  <div id="sidebar">
+    <div id="sidebar_contents">
+      <textarea
+        onChange={props.handleChange}
+        value={props.locationInput}
+        id="address"
+        rows="8"
+        placeholder="Enter one location per line">
+      </textarea>
+      <div id="control_buttons">
+        <button id="createnewquiz" onClick={props.fetchMarkers}>Create Quiz</button><br></br>
+        <button id="savequiz">Save</button>
+        <button id="load_tab_button">Load</button>
       </div>
-    )
-  }
-
-}
+    </div>
+  </div>
+);
 
 export default Sidebar;
