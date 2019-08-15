64332a8b62f2f9778d3f82f9eb790d436be26808
Will Resen <willresen@gmail.com>
2019-08-14 21:21:06 -0700

    Render queston and result state

--- client/src/components/Status.jsx
+++ client/src/components/Status.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Status = () => {
+const Status = (props) => {
   return (
     <div id="status">
       <div id="questioncontainer">
         <div style={{textAlign: "center"}}>
           <button className='button' id='playagain'>Play Again</button>
         </div>
-        <div id="question"><i>There are no markers placed on the map.</i></div>
+        <div id="question"><i>{props.question}</i></div>
       </div>
-      <div id="answer"></div>
+      <div id="answer">{props.result}</div>
     </div>
   );
 };
